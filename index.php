363a0acdcaf61c072657106e75ec4dc4fc918755
sajeeva <sa.hasith@gmail.com>
2018-10-14 11:28:48 +0530

    add index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+    include_once("header.php");
+?>
+
+<div class="container">
+  <div class="row" style="align-items: center;">
+  <div class="col-md-4 col-md-offset-4">
+    <form action="index.php" method="post">
+        click this button to upload a file<button type="submit" class="btn btn-primary">Upload</button>
+    </form>
+    </div>
+  </div>
+</div>
+
+<?php
+    include_once("footer.php");
+?>
