b4275e65da6b3daaf5d21616cc7dac1c83521e9f
sajeeva <sa.hasith@gmail.com>
2018-10-14 12:48:11 +0530

    authentication process (auth_connection.php)

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <div class="container">
   <div class="row" style="align-items: center;">
   <div class="col-md-4 col-md-offset-4">
-    <form action="index.php" method="post">
+    <form action="auth_connection.php" method="post">
         Click this button to upload a file<button type="submit" class="btn btn-primary">Upload</button>
     </form>
     </div>
